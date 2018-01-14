4182c3ab58abb8e2e94fa0c4f82476e9acf8262f
Zlat <lamanskiy.zlat@mail.ru>
2018-01-14 18:47:11 +0700

    научился делать матемматические действия

--- sandbox/src/main/java/ru/plazma/java/study/My1p.java
+++ sandbox/src/main/java/ru/plazma/java/study/My1p.java
@@ -4,6 +4,25 @@ public class My1P {
 
 	public static void main(String[] args) {
 		System.out.println("HELLO,PLAZMA");
+
+		System.out.println(2 + 2);
+		System.out.println(2 * 2);
+		System.out.println(2 / 2);
+		System.out.println(2 - 2);
+
+		System.out.println(1.0 / 2);
+		System.out.println(1 / 2.0);
+		System.out.println(2.0 / 2);
+
+		System.out.println("2" + "2");
+		System.out.println("2" + 2);
+		System.out.println(2 + "2");
+
+		System.out.println(2 + 2 * 2);
+		System.out.println((2 + 2) * 2);
+
+		System.out.println("2 + 2 = " + 2 + 2);
+		System.out.println("2 + 2 = " + (2 + 2));
 	}
 
 
