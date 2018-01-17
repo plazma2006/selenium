5914d2d8706c052ff14d5377344aa2553f42ee4d
Zlat <lamanskiy.zlat@mail.ru>
2018-01-17 19:24:33 +0700

    вывели площадь прямоугольника и многое другое

--- sandbox/src/main/java/ru/plazma/java/study/My1p.java
+++ sandbox/src/main/java/ru/plazma/java/study/My1p.java
@@ -3,15 +3,35 @@ package ru.plazma.java.study;
 public class My1P {
 
 	public static void main(String[] args) {
-		String somebody = "PLAZMA";
-		System.out.println("HELLO, " +  somebody + "!");
+		hello("PLAZMA");
+		hello("USER");
+		hello("ZLAT");
+		hello("WORLD");
 
-		double l = 0.5;
-		double s = l * l;
-		System.out.println("площадь квадрата со стороной " + l + " = " + s);
+		double l = 5;
+		System.out.println(" площадь квадрата со стороной " + l + " = " + area(l));
 
+		double a = 4;
+		double b = 6;
+		System.out.println(" площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
 
 	}
 
+	public static void hello(String somebody) {
+		System.out.println("HELLO, " + somebody + "!");
 
-}
+	}
+
+	public static double area(double len) {
+		return len * len;
+	}
+
+	public static double area(double a, double b) {
+
+		return a * b;
+	}
+	}
+
+
+
+
