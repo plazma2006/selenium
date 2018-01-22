db3e46b27cabfb6881d9de0c5936727751188b1d
Zlat <lamanskiy.zlat@mail.ru>
2018-01-22 14:41:57 +0700

    добавили два класса вычисления площади

--- sandbox/src/main/java/ru/plazma/java/study/My1p.java
+++ sandbox/src/main/java/ru/plazma/java/study/My1p.java
@@ -8,28 +8,18 @@ public class My1P {
 		hello("ZLAT");
 		hello("WORLD");
 
-		double l = 5;
-		System.out.println(" площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println(" площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println(" площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println(" площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
 
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("HELLO, " + somebody + "!");
 
-	}
-
-	public static double area(double len) {
-		return len * len;
-	}
-
-	public static double area(double a, double b) {
-
-		return a * b;
-	}
+		}
 	}
 
 
