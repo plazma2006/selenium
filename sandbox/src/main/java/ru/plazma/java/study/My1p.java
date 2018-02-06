18ae3ac1241109727ae82131bd79917146c4d6d2
Zlat <lamanskiy.zlat@mail.ru>
2018-02-06 17:08:32 +0700

    сделал первый тест

--- sandbox/src/main/java/ru/plazma/java/study/My1p.java
+++ sandbox/src/main/java/ru/plazma/java/study/My1p.java
@@ -14,25 +14,14 @@ public class My1P {
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println(" площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
 
-		point();
-
-	}
+		}
 
 	public static void hello(String somebody) {
 		System.out.println("HELLO, " + somebody + "!");
 
 		}
 
-	public static void point() {
-		double d1, d2;
-		Point p1 = new Point(5, 10);
-		Point p2 = new Point(-5, 10);
-		d1 = p1.distance(p1, p2);
-		d2 = p1.distanceTo(p2);
-		System.out.println(d1);
-		System.out.println(d2);
 
-	}
 }
 
 
